e384e356799baa4bf26e799b5c55039e20839cc4
Robin Hack <hack.robin@gmail.com>
2022-05-03 19:50:15 +0200

    Cleanup code and add return.
    Save previous state of vic.
    Use more safe way how to switch to bitmap mode.
    Wait for key (use KERNAL) and then restore environment.

--- c64/code/main.asm
+++ c64/code/main.asm
@@ -35,10 +35,11 @@
 ;SCREENRAM     = $0400
 
 ; C64
-BGCOLOR       = $d020
-BORDERCOLOR   = $d021
-BASIC         = $0801
-SCREENRAM     = $0400
+BGCOLOR         = $d020
+BORDERCOLOR     = $d021
+BASIC           = $0801
+SCREENRAM       = $0400
+GETIN           = $ffe4
 
 ;==========================================================
 ; BASIC header
@@ -62,9 +63,21 @@ SCREENRAM     = $0400
 
 entry:
 
+    lda $d018
+    pha
 
-    ; change address of screen ram
-    lda #$2
+    ; change address of screen ram - switch vic-II bank
+    ; dirty way
+    ;lda #$2
+    ;sta $dd00
+
+    ; more clear way - just touch desired bites
+    lda $dd00
+    pha
+
+    and #%11111100
+    ; 2 - bank1: $4000-$7fff
+    ora #%00000010
     sta $dd00
 
     ; select bitmap block
@@ -72,10 +85,12 @@ entry:
     sta $d018
 
     ; set screen ram 1000 bytes
-    ldx #0
-    stx BGCOLOR
 
-    lda #%00000001
+    lda BGCOLOR
+    pha
+    lda #0
+    sta BGCOLOR
+
     lda #%00010000
 set_colors:
     sta $5c00, x
@@ -85,11 +100,33 @@ set_colors:
     inx
 bne set_colors
 
-; enable bitmap mode
-lda $d011
-ora #32
-sta $d011
+    ; enable bitmap mode
+    lda $d011
+    pha
+    ora #32
+    sta $d011
 
+    ; wait for key
+input_get:
+    jsr GETIN
+    beq input_get
+
+    ; cleanup
+
+    ; disable bitmap mode and return back to previous mode
+    pla
+    sta $d011
+
+    pla
+    sta BGCOLOR
+
+    ; return bank back
+    pla
+    sta $dd00
+
+    ; return back bitmap block
+    pla
+    sta $d018
 
 rts
 
